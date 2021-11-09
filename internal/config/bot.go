8502a6a0b49e81230e4b346977b030e463b2a97b
r.kuvatov <b2r2@bk.ru>
2021-11-09 21:47:29 +0300

    update

--- internal/config/bot.go
+++ internal/config/bot.go
@@ -6,7 +6,7 @@ type bot struct {
 
 func newBot() *bot {
 	return &bot{
-		//token: "2022598237:AAETE8MNx4qQ71CdsaWtYtvaq0UVyzHBqsw",
-		token: "455278361:AAFSYcbmNvtshujXKU8oxjIxh3XxPyc_pvo", //debug
+		token: "2022598237:AAETE8MNx4qQ71CdsaWtYtvaq0UVyzHBqsw",
+		//token: "455278361:AAFSYcbmNvtshujXKU8oxjIxh3XxPyc_pvo", //debug
 	}
 }
