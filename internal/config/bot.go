cab3ab85f71e90029ead302fa0883c9945e2747f
r.kuvatov <b2r2@bk.ru>
2021-12-15 23:12:31 +0300

    add secret for token

--- internal/config/bot.go
+++ internal/config/bot.go
@@ -6,14 +6,14 @@ import (
 	"github.com/b2r2/link-remover/pkg"
 )
 
-const TOKEN = ".env"
+const filename = ".env"
 
 type bot struct {
 	token string
 }
 
 func newBot() (*bot, error) {
-	token, err := pkg.GetEnv(TOKEN)
+	token, err := pkg.GetEnv(filename)
 	log.Println("TOKEN", token)
 	if err != nil {
 		return nil, err
